0710ade0b1ebf0c8d6bcb63efd986014979418a4
Mohamad Kemal Rasyidin <kemalrasyidin@gmail.com>
2024-06-10 12:17:26 +0700

    Update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,6 +1,5 @@
 <!-- Hero Section Start -->
-<section class="border-l-orange-100 bg-gray-900 bg-cover bg-center bg-no-repeat pb-16 pt-48 bg-blend-multiply"
-    style="background-image: url(img/hero-pattern.svg)">
+<section class="border-l-orange-100 bg-gray-900 bg-cover bg-center bg-no-repeat pb-16 pt-48 bg-blend-multiply">
     <div class="container">
         <div class="mb-20 text-center">
             <p class="mb-2 text-5xl font-bold text-white">{{ $slot }}</p>
